27afaa8ab9d90a5f0571e7d483fcbf6f472e4a2b
Paris Holley <paris@mantisadnetwork.com>
2025-07-23 08:36:31 -0500

    remove unused imports

--- src/components/ObserverComponent.tsx
+++ src/components/ObserverComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const ScrollableItem: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
